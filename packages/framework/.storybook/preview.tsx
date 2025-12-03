ce6e67c63461af44471bcf434bc583b15061b101
Jan Wirth <jottenwe@gmail.com>
2025-12-03 18:20:27 +0100

    snapshot

--- packages/framework/.storybook/preview.tsx
+++ packages/framework/.storybook/preview.tsx
@@ -2,6 +2,10 @@ import type { Preview } from "@storybook/react";
 import React from "react";
 import "../src/index.css";
 import { AudioContextProvider } from "../src/media/audio-context";
+import { Column } from "../src/ui/Column";
+import { Row } from "../src/ui/Row";
+import { FPSMeter } from "@overengineering/fps-meter";
+import { DarkModeToggle } from "../src/ui/DarkModeToggle";
 
 const preview: Preview = {
   parameters: {
@@ -15,9 +19,17 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
+      // <Column className="h-screen">
+      //   <Row>
+      //     <div className="border bg-black">
+      //       <FPSMeter />
+      //     </div>
+      //     <DarkModeToggle />
+      //   </Row>
       <AudioContextProvider>
         <Story />
       </AudioContextProvider>
+      // {/* </Column> */}
     ),
   ],
 };
